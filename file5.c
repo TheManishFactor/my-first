d2f0a13e16c32b05b2e3f4bf94832feab916841e
Manish Ambasta <manishambasta_manish@srmap.edu.in>
2025-01-11 13:23:03 +0530

    Update file5.c

--- file5.c
+++ file5.c
@@ -1,1 +1,27 @@
 
+#include <stdio.h>
+
+int len(char *p) {
+    int count = 0;
+    
+    while (*p != '\0' && *p != '\n') { 
+        count++;
+        p++;
+    }
+    
+    return count;
+}
+
+int main() {
+    char string[10];
+    printf("Enter any word: ");
+    fgets(string, sizeof(string), stdin);
+
+    
+    
+
+    int g = len(string);
+    printf("The size of the given string is: %d\n", g);
+
+    return 0;
+}
