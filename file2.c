9428bdf83d7c803c17e6eb56d4a89956c9c19c37
Manish Ambasta <manishambasta_manish@srmap.edu.in>
2025-01-14 18:27:13 +0530

    Update file2.c

--- file2.c
+++ file2.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
-void count (char *p){
-    int c1=0,c2=0;
-    while(*p !='\0'){
-        if( *p=='A'|| *p=='E'||*p=='I'||*p=='O'||*p=='U'||*p=='a'|| *p=='e'||*p=='i'||*p=='o'||*p=='u'){
-            c1++;
+#include <ctype.h>
+
+void count(char *p) {
+    int c1 = 0, c2 = 0;
+    while (*p != '\0') {
+        if (*p == '\n') { 
+            break;
         }
-            
-        
-        else{
-            
-        
-            c2++;
+        if (isalpha(*p)) { 
+            if (*p == 'A' || *p == 'E' || *p == 'I' || *p == 'O' || *p == 'U' ||
+                *p == 'a' || *p == 'e' || *p == 'i' || *p == 'o' || *p == 'u') {
+                c1++; 
+            } else {
+                c2++; 
+            }
         }
         p++;
-        
     }
-        
-        
-        
-    
-    
-  printf("the number of vowels in the given string is =%d\n ",c1);
-  printf("the number of consonants in the given  string is = %d\n",c2);
-  
+
+    printf("The number of vowels in the given string is = %d\n", c1);
+    printf("The number of consonants in the given string is = %d\n", c2);
 }
 
 int main() {
-    
-    char string[10];
-    printf("enter the string = ");
-    fgets(string,sizeof(string),stdin);
+    char string[100];
+    printf("Enter the string: ");
+    fgets(string, sizeof(string), stdin);
+
     count(string);
-    
-    
-    
 
     return 0;
 }
+
+
+    
+    
+    
